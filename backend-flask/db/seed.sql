1b6a6e955160477cbbf06697a46e4c120089dbd9
Jay <jaybills369@gmail.com>
2023-04-08 09:02:32 +0000

    ddb tested

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK'),
-  ('Jay Kaneki', jaybills369@gmail.com, 'jay' ,'MOCK');
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
