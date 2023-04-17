8a36ebbe0a509dcf9a8710bfd9b6309e8b27951c
Jay <jaybills369@gmail.com>
2023-04-17 15:58:38 +0000

    token expired error

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK'),
-  ('Jay Bells', 'jaybills369@gmail.com', 'Jay' ,'MOCK');
+  ('Jay Bells', 'jaybills369@gmail.com', 'Jay' ,'c7333889-ceee-4011-bd86-c910f1f55ed7');
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
