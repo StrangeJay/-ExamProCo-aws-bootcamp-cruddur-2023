740a21e39524de3c15dfc81258703b36e4b0f3d8
Jay <jaybills369@gmail.com>
2023-04-18 14:42:23 +0000

    Message Error fixed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,9 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  ('Jay Bells', 'jaybills369@gmail.com', 'Jay' ,'c7333889-ceee-4011-bd86-c910f1f55ed7q'),
   ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK'),
-  ('Jay Bells', 'jaybills369@gmail.com', 'Jay' ,'c7333889-ceee-4011-bd86-c910f1f55ed7');
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK');
+  
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
